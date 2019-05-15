6d5933417fdc40e1d34a63aa2bc94db37d7fb5e5
Phạm Đức Cường <cuongphamduc1997@gmail.com>
2019-05-15 15:51:30 +0700

    Update

--- compare.py
+++ compare.py
@@ -43,7 +43,7 @@ class_names = np.unique(y)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=97)
 
-path = 'log'
+path = 'log/weight'
 
 if not os.path.exists(path):
     os.mkdir(path)

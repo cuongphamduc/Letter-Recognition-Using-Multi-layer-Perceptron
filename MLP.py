1f127d338e0cb8ec21c9fdddd01c366b751b73fd
Phạm Đức Cường <cuongphamduc1997@gmail.com>
2019-05-16 20:44:18 +0700

    Update MLP.py

--- MLP.py
+++ MLP.py
@@ -20,7 +20,7 @@ def to_one_hot(vector, dimension=26):
     return res
 
 
-def check_lost(arr_loss, delta, number):
+def check_loss(arr_loss, delta, number):
     if len(arr_loss) < 11:
         return False
 
@@ -50,10 +50,14 @@ def cross_entropy_loss(y, y_pre):
 
 class MLP:
     def __init__(self, d0, d1, d2, learning_rate, tol, n_iter_no_change, batch_size, num_epochs):
-        self.W1 = 0.01 * np.random.randn(d0, d1)  # W1 = (16, 100)
-        self.b1 = np.zeros((d1, 1))  # b1 = (100, 1)
-        self.W2 = 0.01 * np.random.randn(d1, d2)  # W2 = (100, 26)
-        self.b2 = np.zeros((d2, 1))  # b2 = (26, 1)
+        self.W1 = 0.01 * np.random.randn(d0, d1)
+        # W1 = (16, 100)
+        self.b1 = np.zeros((d1, 1))
+        # b1 = (100, 1)
+        self.W2 = 0.01 * np.random.randn(d1, d2)
+        # W2 = (100, 26)
+        self.b2 = np.zeros((d2, 1))
+        # b2 = (26, 1)
         self.learning_rate = learning_rate
         self.tol = tol
         self.n_iter_no_change = n_iter_no_change
@@ -67,46 +71,67 @@ class MLP:
         with tqdm(total=self.num_epochs, file=sys.stdout, desc='Training') as pbar:
             for epoch in range(self.num_epochs):
                 tmp = []
-                X = X.T  # X = (16000, 16)
-                y = y.T  # y =(16000, 16)
+                X = X.T
+                # X = (16000, 16)
+                y = y.T
+                # y =(16000, 16)
 
                 X, y = sklearn.utils.shuffle(X, y)
 
-                X = X.T  # X = (16, 16000)
-                y = y.T  # y =(26, 16000)
+                X = X.T
+                # X = (16, 16000)
+                y = y.T
+                # y =(26, 16000)
 
                 for j in range(0, N, self.batch_size):
                     # Pick a mini-batch
-                    X_batch = X[:, j: j + self.batch_size]  # X_batch = (16, 50)
-                    y_batch = y[:, j: j + self.batch_size]  # y_batch =(26, 50)
+                    X_batch = X[:, j: j + self.batch_size]
+                    # X_batch = (16, 50)
+                    y_batch = y[:, j: j + self.batch_size]
+                    # y_batch =(26, 50)
 
                     # Feedforward
-                    Z1 = np.dot(self.W1.T, X_batch) + self.b1  # Z1 = (100, 16) x (16, 50) + (100, 1) = (100, 50)
-                    A1 = np.maximum(Z1, 0)  # A1 = (100, 50)
-                    Z2 = np.dot(self.W2.T, A1) + self.b2  # Z2 = (26, 100) x (100, 50) + (26, 1) = (26, 50)
-                    y_predict = softmax(Z2)  # y_predict = (26, 50)
+                    Z1 = np.dot(self.W1.T, X_batch) + self.b1
+                    # Z1 = (100, 16) x (16, 50) + (100, 1) = (100, 50)
+                    A1 = np.maximum(Z1, 0)
+                    # A1 = (100, 50)
+                    Z2 = np.dot(self.W2.T, A1) + self.b2
+                    # Z2 = (26, 100) x (100, 50) + (26, 1) = (26, 50)
+                    y_predict = softmax(Z2)
+                    # y_predict = (26, 50)
 
                     tmp.append(cross_entropy_loss(y_batch, y_predict))
 
                     # Backpropagation
-                    E2 = (y_predict - y_batch) / N  # E2 = (26, 50)
-                    dW2 = np.dot(A1, E2.T)  # dW2 = (100, 50) x (50, 26) = (100, 26)
-                    db2 = np.sum(E2, axis=1, keepdims=True)  # db2 = (26, 1)
-                    E1 = np.dot(self.W2, E2)  # E1 = (100, 26) x (26, 50) = (100, 50)
-                    E1[Z1 <= 0] = 0  # E1 = (100, 50)
-                    dW1 = np.dot(X_batch, E1.T)  # dW1 = (16, 50) x (50, 100) = (16, 100)
-                    db1 = np.sum(E1, axis=1, keepdims=True)  # db1 = (100, 1)
+                    E2 = (y_predict - y_batch) / N
+                    # E2 = (26, 50)
+                    dW2 = np.dot(A1, E2.T)
+                    # dW2 = (100, 50) x (50, 26) = (100, 26)
+                    db2 = np.sum(E2, axis=1, keepdims=True)
+                    # db2 = (26, 1)
+                    E1 = np.dot(self.W2, E2)
+                    # E1 = (100, 26) x (26, 50) = (100, 50)
+                    E1[Z1 <= 0] = 0
+                    # E1 = (100, 50)
+                    dW1 = np.dot(X_batch, E1.T)
+                    # dW1 = (16, 50) x (50, 100) = (16, 100)
+                    db1 = np.sum(E1, axis=1, keepdims=True)
+                    # db1 = (100, 1)
 
                     # Mini-batch gradient descent update
-                    self.W1 += -self.learning_rate * dW1  # W1 = (16, 100)
-                    self.b1 += -self.learning_rate * db1  # b1 = (100, 1)
-                    self.W2 += -self.learning_rate * dW2  # W2 = (100, 26)
-                    self.b2 += -self.learning_rate * db2  # b2 = (26, 1)
+                    self.W1 += -self.learning_rate * dW1
+                    # W1 = (16, 100)
+                    self.b1 += -self.learning_rate * db1
+                    # b1 = (100, 1)
+                    self.W2 += -self.learning_rate * dW2
+                    # W2 = (100, 26)
+                    self.b2 += -self.learning_rate * db2
+                    # b2 = (26, 1)
 
                 loss.append(statistics.mean(tmp))
                 pbar.write('Iteration %d, loss = %f' % (epoch + 1, loss[-1]))
 
-                if check_lost(loss, self.tol, self.n_iter_no_change):
+                if check_loss(loss, self.tol, self.n_iter_no_change):
                     pbar.write('Training loss did not improve more than tol=%f'
                                'for %d consecutive epochs. Stopping.'
                                % (self.tol, self.n_iter_no_change))
@@ -117,9 +142,12 @@ class MLP:
                 sleep(1)
 
     def predict(self, X):
-        Z1 = np.dot(self.W1.T, X) + self.b1  # Z1 = (100, 16) x (16, 4000) + (100, 1) = (100, 4000)
-        A1 = np.maximum(Z1, 0)  # A1 = (100, 4000)
-        Z2 = np.dot(self.W2.T, A1) + self.b2  # Z2 = (26, 100) x (100, 4000) + (26, 1) = (26, 4000)
+        Z1 = np.dot(self.W1.T, X) + self.b1
+        # Z1 = (100, 16) x (16, 4000) + (100, 1) = (100, 4000)
+        A1 = np.maximum(Z1, 0)
+        # A1 = (100, 4000)
+        Z2 = np.dot(self.W2.T, A1) + self.b2
+        # Z2 = (26, 100) x (100, 4000) + (26, 1) = (26, 4000)
 
         return np.argmax(Z2, axis=0)  # = (4000,)
 
@@ -136,14 +164,18 @@ class MLP:
 if __name__ == '__main__':
     df = pd.read_csv('data/data.csv', header=None)
 
-    label = df[0]  # label = (20000,)
-    feature = df.drop(0, axis=1)  # feature = (20000, 16)
+    label = df[0]
+    # label = (20000,)
+    feature = df.drop(0, axis=1)
+    # feature = (20000, 16)
 
     X_train, X_test, y_train, y_test = train_test_split(feature, label, test_size=0.2, random_state=97)
     # X_train = (16000, 16), X_test  = (4000, 16), y_train = (16000,), y_test = (4000,)
 
-    X_train = np.asarray(X_train).T  # X = (16, 16000)
-    y_train = to_one_hot(y_train).T  # y =(26, 16000)
+    X_train = np.asarray(X_train).T
+    # X = (16, 16000)
+    y_train = to_one_hot(y_train).T
+    # y =(26, 16000)
 
     model = MLP(d0=16, d1=100, d2=26, learning_rate=1, tol=0.0001,
                 n_iter_no_change=10, batch_size=50, num_epochs=2000)
@@ -151,10 +183,13 @@ if __name__ == '__main__':
     model.fit(X_train, y_train)
     model.save('log/weight')
 
-    X_test = np.asarray(X_test).T  # X_test = (16, 4000)
-    predict = model.predict(X_test)  # predict = (4000,)
+    X_test = np.asarray(X_test).T
+    # X_test = (16, 4000)
+    predict = model.predict(X_test)
+    # predict = (4000,)
 
-    y_test = np.asarray(y_test)  # y_test = (4000,)
+    y_test = np.asarray(y_test)
+    # y_test = (4000,)
 
     for ind, val in enumerate(y_test):
         y_test[ind] = ord(val) - ord('A')
